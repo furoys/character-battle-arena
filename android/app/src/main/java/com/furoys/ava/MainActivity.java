a87c4b438ad61aa88f764587ef99200eda4c9f10
furoys <furoys@gmail.com>
2026-05-09 17:42:01 +0000

    Add Capacitor Android project for AVA

---
+++ android/app/src/main/java/com/furoys/ava/MainActivity.java
@@ -0,0 +1,5 @@
+package com.furoys.ava;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.furoys.ava',
+  appName: 'A.V.A',
+  webDir: 'artifacts/fight-club/dist/public'
+};
+
+export default config;
